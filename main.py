3674f2dbe87f43f4dc1948eac3a93880a5c68e2c
Yulia Savanova <yulia@Yulias-iMac.local>
2019-05-01 06:06:55 +0300

    filtered by name and sorted by price

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import csv
 import re
 from pprint import pprint
 
-from pymongo import MongoClient
 import pymongo
+# from pymongo import MongoClient
 
 import json
-from collections import OrderedDict
 
 
-mongo_client = MongoClient()
-mongo_db = mongo_client['db']
-tickets_collection = mongo_db['tickets']
+mongo_client = pymongo.MongoClient()
 
 
 def read_data(csv_file, db):
     """
     Загрузить данные в бд из CSV-файла
     """
+    mongo_db = mongo_client[db]
+    tickets_collection = mongo_db['tickets']
+
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
         reader = csv.DictReader(csvfile)
@@ -27,9 +27,9 @@ def read_data(csv_file, db):
 
         mongo_db = mongo_client[db]
 
-        result = tickets_collection.insert_many(output_dict)
+        tickets_collection.insert_many(output_dict)
 
-    return result
+    return tickets_collection
 
 
 def find_cheapest(db):
@@ -38,6 +38,7 @@ def find_cheapest(db):
     Документация: https://docs.mongodb.com/manual/reference/method/cursor.sort/
     """ 
     mongo_db = mongo_client[db]
+
     ordered_collection = mongo_db.tickets_collection.find().sort('Цена', pymongo.ASCENDING)
 
     return ordered_collection
@@ -48,15 +49,21 @@ def find_by_name(name, db):
     Найти билеты по имени исполнителя (в том числе – по подстроке),
     и вернуть их по возрастанию цены
     """
+    mongo_db = mongo_client[db]
 
-    regex = re.compile('укажите регулярное выражение для поиска. ' \
-                       'Обратите внимание, что в строке могут быть специальные символы, их нужно экранировать')
+    regex = re.compile('[\S ]*.*' + str(name) + '[\S ]*.*')
+    tickets_list = list(tickets_collection.find({'Исполнитель': {'$regex': regex, '$options': '-i'}}))
+    tickets_list_sorted = sorted(tickets_list, key=lambda k: int(k['Цена']))
+
+    return tickets_list_sorted
 
 
 if __name__ == '__main__':
-    tickets = read_data('artists.csv', 'concerts-db')
-    # pprint(tickets.inserted_ids)
+    tickets_collection = read_data('artists.csv', 'concerts-db')
+    pprint(list(tickets_collection.find()))
 
     tickets_by_price = find_cheapest('concerts-db')
-    # print(tickets_by_price)
+    print(tickets_by_price)
 
+    tickets_list_sorted = find_by_name('а', 'concerts-db')
+    pprint(tickets_list_sorted)
